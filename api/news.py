55109f922c8f611141b5c13559975dcbde087819
dexter-mazoe <73579692+dexter-mazoe@users.noreply.github.com>
2021-02-22 20:48:59 +0200

    Update news.py

--- api/news.py
+++ api/news.py
@@ -1,56 +1,31 @@
+import json
+from integrator import news_articles
+
+
 class NewsAgent:
-    def search_by_title(self, title) :
-        """ Returns a dictionary of news articles
+    
+	data = news_articles()
+	def search_by_title(self, title):
+		response = []
+		for article in self.data:
+			article = json.loads(article)
+			if title.lower() in article['title'].lower():
+				response.append(article)
+		print('results: ',len(response), title)
+		return response
 
-            For example:
+	def search_by_category(self, cat):
+		cat = str(cat)
+		response = []
+		for article in self.data:
+			article = json.loads(article)
+			if cat == 'Opinion and Analysis':
+				cat = 'Opinion and analysis'				
+			if cat == article['cat']:
+				response.append(article)
+		print('results: ',len(response), cat)
+		return response
 
-            return [
-                {
-                    title: "Lockdown: Level 4 extended by two weeks",
-                    desc: "Herald Reporters The national lockdown will continue at level four for a further two weeks to February 15 to consolidate ... ",
-                    link: "https://www.herald.co.zw/zinara-toll-fees-to-move-with-exchange-rate/"
-                },
-                {
-                    title: "Zinara toll fees to move with exchange rate",
-                    desc: "Herald Reporters The national lockdown will continue at level four for a further two weeks to February 15 to consolidate ... ",
-                    link: "https://www.herald.co.zw/zinara-toll-fees-to-move-with-exchange-rate/"
-                },
-            ]
-        
-        """
-        return [
-                    {
-                        'title': "Lockdown: Level 4 extended by two weeks",
-                        'desc': "Herald Reporters The national lockdown will continue at level four for a further two weeks to February 15 to consolidate ... ",
-                        'link': "https://www.herald.co.zw/zinara-toll-fees-to-move-with-exchange-rate/"
-                    },
-                    {
-                        'title': "Zinara toll fees to move with exchange rate",
-                        'desc': "Herald Reporters The national lockdown will continue at level four for a further two weeks to February 15 to consolidate ... ",
-                        'link': "https://www.herald.co.zw/zinara-toll-fees-to-move-with-exchange-rate/"
-                    },
-                ]
-
-    def search_by_category(self, cat) :
-        """ Returns a dictionary of news articles
-
-            For example:
-
-            return [
-                {
-                    title: "Lockdown: Level 4 extended by two weeks",
-                    desc: "Herald Reporters The national lockdown will continue at level four for a further two weeks to February 15 to consolidate ... ",
-                    link: "https://www.herald.co.zw/zinara-toll-fees-to-move-with-exchange-rate/"
-                },
-                {
-                    title: "Zinara toll fees to move with exchange rate",
-                    desc: "Herald Reporters The national lockdown will continue at level four for a further two weeks to February 15 to consolidate ... ",
-                    link: "https://www.herald.co.zw/zinara-toll-fees-to-move-with-exchange-rate/"
-                },
-            ]
-        
-        """
-        raise NotImplementedError
 
 
 
